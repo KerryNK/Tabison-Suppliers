97e1fda3b63dd8b9f6bf13aba5e53b9c7f61e1b7
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-07-21 04:19:22 +0300

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import SuppliersPage from "./pages/SuppliersPage";
@@ -11,20 +11,18 @@ import { AuthProvider } from "./context/AuthContext";
 
 const App: React.FC = () => (
   <AuthProvider>
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/suppliers/*" element={<SuppliersPage />} />
-          <Route path="/products/*" element={<ProductsPage />} />
-          <Route path="/orders/*" element={<OrdersPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/suppliers/*" element={<SuppliersPage />} />
+        <Route path="/products/*" element={<ProductsPage />} />
+        <Route path="/orders/*" element={<OrdersPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Layout>
   </AuthProvider>
 );
 
-export default App; 
+export default App;
