7fdbc2b00f51025aac217ae33051bcf9af9b0a51
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-07-26 07:02:43 +0300

    commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is correctly set!
+  root: './', // ensure this matches the location of index.html
   build: {
-    outDir: 'dist', // Make sure this matches your vercel.json
-  },
-});
+    outDir: 'dist',
+  }
+})
