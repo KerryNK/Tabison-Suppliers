544663fce37eb4f30c6a2d415dd88c2efb97af84
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-08-09 16:37:06 +0300

    commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,21 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
+    proxy: command === 'serve' ? {
       "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
-    },
+    } : undefined,
   },
   build: {
     outDir: "dist",
     sourcemap: true,
   },
-})
+}))
