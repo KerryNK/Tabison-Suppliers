daf75ae69ced5f4d71474963ceb92caf34e5f3e5
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-08-28 21:02:37 +0300

    commit

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: https://suppliers-7zjy.onrender.com
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_SUPPLIER_ID: string;
+  readonly VITE_SUPPLIER_NAME: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
