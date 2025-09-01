e2ad71ca572e85a63178a3eb485c76c559110691
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-09-01 16:03:11 +0300

    commit

--- backend/validation/schema.js
+++ backend/validation/schema.js
@@ -4,7 +4,6 @@ export const registerSchema = Joi.object({
   name: Joi.string().min(2).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  // add other registration fields as needed
 });
 
 export const verifyOTPSchema = Joi.object({
