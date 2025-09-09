a8df45d0fadb1bef3a860c220bb6b217234ce7c7
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-09-09 14:49:56 +0300

    commit

--- frontend/src/pages/authPage.tsx
+++ frontend/src/pages/authPage.tsx
@@ -1,13 +1,13 @@
 // src/pages/AuthPage.tsx
-import React from 'react';
+import React from "react";
 
 interface AuthPageProps {
-  mode: 'login' | 'register' | 'forgot-password' |'reset-password';
+  mode: "login" | "register" | "forgot-password" | "reset-password";
 }
 
 const AuthPage: React.FC<AuthPageProps> = ({ mode }) => {
   return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
+    <div style={{ padding: "50px", textAlign: "center" }}>
       <h2>Authentication - {mode}</h2>
       <p>This is a placeholder for the {mode} functionality.</p>
     </div>
