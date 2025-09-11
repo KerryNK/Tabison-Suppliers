16b482f485c82bd66b295c008e191d0d5a941d22
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-09-12 02:37:13 +0300

    commit

--- frontend/src/ui/tooltip.tsx
+++ frontend/src/ui/tooltip.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "@/frontend/src/utils"
+import { cn } from "@frontend/src/utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
 
